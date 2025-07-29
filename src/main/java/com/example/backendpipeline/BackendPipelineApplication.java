87062ca50759cd02aef8e4ebf66056c00ec325fc
Bahae <bahaeddine_moutaouikil@um5.ac.ma>
2025-07-29 11:11:49 +0100

    Initial commit: Spring Boot skeleton

---
+++ src/main/java/com/example/backendpipeline/BackendPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendpipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPipelineApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BackendPipelineApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/backendpipeline/BackendPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendpipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPipelineApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
